3fb31d61ad3a0599545cab3bf590b4aaaf6cc4b1
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-08-07 16:29:14 +0300

    Edit of connection

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -7,9 +7,8 @@ if(!process.env.DB_URL) {
     throw new Error('Error in DB_URL');
 }
 
-const params = JSON.parse(process.env.DB_URL);
+const params = url.parse(process.env.DB_URL);
 const [username, password] = params.auth.split(':');
-
 const options = {
   host: params.hostname,
   port: params.port,
