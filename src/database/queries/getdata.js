59cd01934eefc7264ccb675a9782f32142af5bea
lubnaabad <lubna_mosa@hotmail.com>
2018-08-07 16:10:19 +0300

    get data from database #4

---
+++ src/database/queries/getdata.js
@@ -0,0 +1,9 @@
+const dbconnection =require('../db_connection');
+const sql ={text:'SELECT * FROM books;'};
+
+const getdata = (cb) => dbconnection.query(sql, (err,res) => {
+    if (err) {
+        cb(err);
+    }
+    cb(null, res);
+});
--- src/router.js
+++ src/router.js
@@ -1,18 +1,21 @@
 const {
-    handelHomePage,
-    serverStaticFile,
-    handelError,
-  } = require('./handler/functions.js');
-  
-  const router = (req, res) => {
-    const endponit = req.url;
-    if (endponit === '/') {
-      handelHomePage(req, res);
-    } else if (endponit.includes('public')) {
-      serverStaticFile(req, res);
-    } else {
-      handelError(res);
-    }
-  };
-  
-  module.exports = router;
+  handelHomePage,
+  serverStaticFile,
+  handelError,
+  getbooks
+} = require("./handler/functions.js");
+
+const router = (req, res) => {
+  const endponit = req.url;
+  if (endponit === "/") {
+    handelHomePage(req, res);
+  } else if (endponit.includes("public")) {
+    serverStaticFile(req, res);
+  } else if (endponit == "/getbooks") {
+    getbooks(req, res);
+  } else {
+    handelError(res);
+  }
+};
+
+module.exports = router;
