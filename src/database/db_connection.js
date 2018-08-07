8a6d3f5da25111ed88f1be8e171388ac251ac909
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-08-07 15:39:42 +0300

    Connection databas #4

---
+++ src/database/db_connection.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const url = require('url');
+require('env2')('./config.env');
+
+if(!process.env.DB_URL) {
+    throw new Error('Error in DB_URL');
+}
+
+const params = JSON.parse(process.env.DB_URL);
+const [username, password] = params.auth.split(':');
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  user: username,
+  password,
+  ssl: true,
+}
+
+module.exports = new Pool(options);
